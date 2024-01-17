b16f21eaef49bfae50ad101ab30a20b97e937d66
Cedric <99885260+Cenikl@users.noreply.github.com>
2024-01-17 10:01:12 +0300

    feat: initial files

---
+++ src/main/java/com/genfood/foodgenback/FoodGenBackApplication.java
@@ -0,0 +1,13 @@
+package com.genfood.foodgenback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodGenBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FoodGenBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/genfood/foodgenback/FoodGenBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.genfood.foodgenback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodGenBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
